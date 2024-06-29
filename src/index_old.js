bde4432c8c4003cbfe2c805ff36ce8fa22c9b562
Ivan Trifonov <ivan14trifonov@gmail.com>
2024-06-29 14:20:09 +0300

    Non-integrated authorization

--- src/index_old.js
+++ src/index_old.js
@@ -1,45 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import {createBrowserRouter, Navigate, RouterProvider} from "react-router-dom";
-import Login from "./routes/login";
-import Register from "./routes/register";
-import User from "./routes/user";
-import ErrorPage from "./error-page";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Navigate to="login"/>,
-    errorElement: <ErrorPage/>,  // new
-  },
-  {
-    path: "/",
-    element: <Navigate to="login"/>,
-  },
-  {
-    path: "/login",
-    element: <Login/>,
-  },
-  {
-    path: "/register",
-    element: <Register/>,
-  },
-  {
-    path: "/user",
-    element: <User/>,
-  },
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <App />
   </React.StrictMode>
 );
 
