f01e349b7c0cd84dc579784b3dbeb4c806d3f468
Ivan Trifonov <ivan14trifonov@gmail.com>
2024-06-29 17:52:01 +0300

    Integrated authorization v1.4

--- src/firebase.js
+++ src/firebase.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
 
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 
 export const createUser = async (email, password) => {
   return createUserWithEmailAndPassword(getAuth(app), email, password);
