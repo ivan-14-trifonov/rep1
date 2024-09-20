2fc862ecc30de29de6b115db940686fad54ef12c
Ivan Trifonov <ivan14trifonov@mail.ru>
2024-09-20 15:21:21 +0300

    Авторизация гугл 1.2

---
+++ src/routes/login.jsx
@@ -0,0 +1,53 @@
+import {Alert, Box, Button, Container, Link, TextField, Typography} from "@mui/material";
+import {useNavigate} from "react-router-dom";
+import {useState} from "react";
+import {signInUser} from "../firebase";
+import {startSession} from "../session";
+
+export default function Login() {
+
+  const navigate = useNavigate();
+
+  const [error, setError] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    // validate the inputs
+    if (!email || !password) {
+      setError("Please enter your username and password.");
+      return;
+    }
+
+    // clear the errors
+    setError("");
+
+    // TODO: send the login request
+    try {
+      let loginResponse = await signInUser(email, password);
+      startSession(loginResponse.user);
+      navigate("/user");
+    } catch (error) {
+      console.error(error.message);
+      setError(error.message);
+    }
+  }
+
+  return (
+    <Root>
+      <p>Войдите через Google для начала работы</p>
+      <Button
+        type="primary"
+        shape="round"
+        icon={<GoogleOutlined />}
+        size="large"
+        loading={isLoading}
+        onClick={handleAuth}
+      >
+        Войти через Google
+      </Button>
+    </Root>
+  )
+}
