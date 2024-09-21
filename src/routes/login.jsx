01d2f0c7d665e13656c85a2bca461877f07a282a
Ivan Trifonov <ivan14trifonov@gmail.com>
2024-09-21 12:00:22 +0300

    Авторизация через гугл 1.19

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -4,15 +4,16 @@ import {useState} from "react";
 import {signInUser} from "../firebase";
 import {startSession} from "../session";
 
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import {GoogleOutlined} from "@ant-design/icons";
 
-const Root = styled("div")`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-`;
+// const Root = styled("div")`
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   align-items: center;
+//   height: 100%;
+// `;
 
 export default function Login() {
 
