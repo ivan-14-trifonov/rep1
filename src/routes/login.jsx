7127be7c136b47a4dd232982eee2d3f64f19898a
Ivan Trifonov <ivan14trifonov@gmail.com>
2024-09-21 16:54:15 +0300

    Авторизация через гугл 1.32

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -14,6 +14,38 @@ export default function Login() {
   signInWithRedirect(auth, provider);
 
 
-  return 0;
+  return (
+    <Container maxWidth="xs" sx={{mt: 2}}>
+      <Typography variant="h5" component="h1" gutterBottom textAlign="center">
+        Login
+      </Typography>
+      {error && <Alert severity="error" sx={{my: 2}}>{error}</Alert>}
+      <Box component="form" onSubmit={onSubmit}>
+        <TextField
+          label="Email"
+          variant="outlined"
+          autoComplete="email"
+          value={email}
+          // onChange={(e) => setEmail(e.target.value)}
+          sx={{mt: 1}}
+          fullWidth
+        />
+        <TextField
+          label="Password"
+          variant="outlined"
+          type="password"
+          autoComplete="new-password"
+          value={password}
+          // onChange={(e) => setPassword(e.target.value)}
+          sx={{mt: 3}}
+          fullWidth
+        />
+        <Button variant="contained" type="submit" sx={{mt: 3}} fullWidth>Login</Button>
+        <Box sx={{mt: 2}}>
+          Don't have an account yet? <Link href="/register">Register</Link>
+        </Box>
+      </Box>
+    </Container>
+  )
 
 }
