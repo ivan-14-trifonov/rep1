ced4f85268f794d10d96df3ca57bf85f8f3f8397
Ivan Trifonov <ivan14trifonov@gmail.com>
2024-09-21 16:26:19 +0300

    Авторизация через гугл 1.30

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -4,35 +4,14 @@ import {useState} from "react";
 import {signInUser} from "../firebase";
 import {startSession} from "../session";
 
-import {getAuth, getRedirectResult, GoogleAuthProvider} from "firebase/auth";
+import { getAuth, signInWithRedirect } from "firebase/auth";
 
 export default function Login() {
 
-  const provider = new GoogleAuthProvider();
-
   const auth = getAuth();
-  getRedirectResult(auth)
-    .then((result) => {
-      // This gives you a Google Access Token. You can use it to access Google APIs.
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-
-      // The signed-in user info.
-      const user = result.user;
-      // IdP data available using getAdditionalUserInfo(result)
-      // ...
-    }).catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.customData.email;
-      // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
-    });
+  signInWithRedirect(auth, provider);
 
 
   return 0;
-  
+
 }
